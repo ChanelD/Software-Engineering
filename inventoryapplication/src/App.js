52fb9a61e3a63fca8b3d5fe4eddf18ca40a23442
Chanel Dang <chaneldang8@gmail.com>
2026-07-10 04:27:37 -0400

    Add full project files

---
+++ inventoryapplication/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="title-page">
+      <h1>Inventory Manager</h1>
+      <p>Keep track of your stock, simply and easily.</p>
+    </div>
+  );
+}
+
+export default App;
