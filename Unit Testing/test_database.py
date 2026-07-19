a2e7f7fbf2226ebe9c27f2215f356a7479640e62
Mikhail Tsegaye <mikhailtsegaye3@gmail.com>
2026-07-19 19:25:03 -0400

    Update test_database.py

--- Unit Testing/test_database.py
+++ Unit Testing/test_database.py
@@ -1,2 +1,28 @@
 import py test
 import psycopg2
+
+def get_connection():
+  return psycop2.connect(
+    ""postgresql://postgres:/*mMq2!F6gLcHV,@db.nmikzhnpiukcelaniqrn.supabase.co:5432/postgres"
+    )
+
+
+def test_add_inventory_item():
+  conn = get_connection()
+  cursor = conn.cursor()
+
+  cursor.execute("""
+    INSERT INTO Inventory (name, categiry, quantity, low_stock_threshold)
+    VALUES ('Not a real product', 'Coffee', 50, 10)
+    Returning item_id, name, quantity
+    """)
+
+    result = cursor.fetchone()
+
+    assert result is not None
+    assert result[1] == 'Test Coffee Beans'
+    assert result[2] == 50
+
+    conn.rollback()
+    cursor.close()
+    conn.close()
