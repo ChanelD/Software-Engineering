22bcb9c8e3f1fcb131a15e3e16c4e1290f8c6da4
Mikhail Tsegaye <mikhailtsegaye3@gmail.com>
2026-07-20 20:17:12 -0400

    Updated unit test for test_database.py

--- Unit Testing/test_database.py
+++ Unit Testing/test_database.py
@@ -2,27 +2,23 @@ import pytest
 import psycopg2
 
 def get_connection():
-  return psycop2.connect(
-    ""postgresql://postgres:/*mMq2!F6gLcHV,@db.nmikzhnpiukcelaniqrn.supabase.co:5432/postgres"
+    return psycopg2.connect(
+        host = 'db.nmikzhnpiukcelaniqrn.supabase.co',
+        port = 5432,
+        database = 'postgres',
+        user = 'postgres',
+        password = '/*mMq2!F6gLcHV,'
     )
-
-
+ 
 def test_add_inventory_item():
-  conn = get_connection()
-  cursor = conn.cursor()
-
-  cursor.execute("""
-    INSERT INTO Inventory (name, category, quantity, low_stock_threshold)
-    VALUES ('Not a real product', 'Coffee', 50, 10)
-    Returning item_id, name, quantity
-    """)
-
+    conn = get_connection()
+    cursor = conn.cursor()
+    sql = 'INSERT INTO Inventory (name, category, quantity, low_stock_threshold) VALUES (%s, %s, %s, %s) RETURNING item_id, name, quantity'
+    cursor.execute(sql, ('Not a real product', 'Coffee', 50, 10))
     result = cursor.fetchone()
-
     assert result is not None
     assert result[1] == 'Not a real product'
     assert result[2] == 50
-
     conn.rollback()
     cursor.close()
     conn.close()
